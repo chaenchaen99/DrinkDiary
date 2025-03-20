e1234f1ef7aaf05cc64f55bb0202396f6e9621a3
chaenchaen99 <chaenjung.99@gmail.com>
2025-03-20 22:52:10 +0900

    searchbar ui  퍼블리싱

--- lib/features/home/widgets/home_appbar.dart
+++ lib/features/home/widgets/home_appbar.dart
@@ -1,10 +1,51 @@
 import 'package:drink_diary/features/home/providers/category_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../../core/constants/app_colors.dart';
 import '../../../core/constants/app_sizes.dart';
+import '../../../shared/widgets/custom_search_bar.dart';
 
-class HomeAppbar extends StatelessWidget implements PreferredSizeWidget {
-  const HomeAppbar({super.key});
+class HomeAppbar extends StatefulWidget implements PreferredSizeWidget {
+  const HomeAppbar({
+    super.key,
+  });
+
+  @override
+  State<HomeAppbar> createState() => _HomeAppbarState();
+
+  @override
+  Size get preferredSize => const Size.fromHeight(44);
+}
+
+class _HomeAppbarState extends State<HomeAppbar> {
+  late bool _isSearch;
+  late TextEditingController _searchController;
+
+  @override
+  void initState() {
+    super.initState();
+    _isSearch = false;
+    _searchController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _searchController.dispose();
+  }
+
+  void _showSearchAppBar(BuildContext context) {
+    setState(() {
+      _isSearch = true;
+    });
+  }
+
+  void _hideSearchAppBar(BuildContext context) {
+    setState(() {
+      _isSearch = false;
+      _searchController.clear();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,61 +65,70 @@ class HomeAppbar extends StatelessWidget implements PreferredSizeWidget {
               fit: BoxFit.contain, // 이미지 비율을 유지하며 크기 조정
             ),
           ),
-          title: AnimatedContainer(
-            duration: const Duration(milliseconds: 400),
-            decoration: BoxDecoration(
-              borderRadius: const BorderRadius.all(
-                Radius.circular(AppSizes.radius16),
-              ),
-              color: state.theme.containerColor,
-            ),
-            child: SizedBox(
-              height: 32,
-              width: 144,
-              child: DefaultTabController(
-                initialIndex: state.index,
-                length: DrinkCategory.values.length,
-                child: TabBar(
-                  onTap: (index) => ref
-                      .read(categoryNotifierProvider.notifier)
-                      .changeIndex(index),
-                  indicatorSize: TabBarIndicatorSize.tab,
-                  indicatorWeight: 0,
-                  dividerHeight: 0,
-                  labelPadding:
-                      const EdgeInsets.only(left: 12, right: 12, top: 4),
-                  labelColor: state.theme.labelColor,
-                  dividerColor: Colors.transparent,
-                  labelStyle: Theme.of(context).textTheme.titleMedium,
-                  unselectedLabelColor: state.theme.unselectedLabelColor,
-                  unselectedLabelStyle: Theme.of(context).textTheme.titleMedium,
-                  overlayColor: WidgetStateProperty.all(Colors.transparent),
-                  indicator: BoxDecoration(
+          title: _isSearch
+              ? SearchInputTextfield(
+                  controller: _searchController,
+                  onClose: () => _hideSearchAppBar(context),
+                )
+              : AnimatedContainer(
+                  duration: const Duration(milliseconds: 400),
+                  decoration: BoxDecoration(
                     borderRadius: const BorderRadius.all(
                       Radius.circular(AppSizes.radius16),
                     ),
-                    color: state.theme.indicatorColor,
+                    color: state.theme.containerColor,
                   ),
-                  tabs: List.generate(
-                    DrinkCategory.values.length,
-                    (index) => Tab(
-                      text: DrinkCategory.values[index].toName,
+                  child: SizedBox(
+                    height: 32,
+                    width: 144,
+                    child: DefaultTabController(
+                      initialIndex: state.index,
+                      length: DrinkCategory.values.length,
+                      child: TabBar(
+                        onTap: (index) => ref
+                            .read(categoryNotifierProvider.notifier)
+                            .changeIndex(index),
+                        indicatorSize: TabBarIndicatorSize.tab,
+                        indicatorWeight: 0,
+                        dividerHeight: 0,
+                        labelPadding:
+                            const EdgeInsets.only(left: 12, right: 12),
+                        labelColor: state.theme.labelColor,
+                        dividerColor: Colors.transparent,
+                        labelStyle: Theme.of(context).textTheme.titleMedium,
+                        unselectedLabelColor: state.theme.unselectedLabelColor,
+                        unselectedLabelStyle:
+                            Theme.of(context).textTheme.titleMedium,
+                        overlayColor:
+                            WidgetStateProperty.all(Colors.transparent),
+                        indicator: BoxDecoration(
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(AppSizes.radius16),
+                          ),
+                          color: state.theme.indicatorColor,
+                        ),
+                        tabs: List.generate(
+                          DrinkCategory.values.length,
+                          (index) => Tab(
+                            text: DrinkCategory.values[index].toName,
+                          ),
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ),
-          ),
           actions: [
-            GestureDetector(
-              onTap: () {},
-              child: Container(
-                margin: const EdgeInsets.only(right: 16),
-                width: 32,
-                height: 32,
-                child: const Icon(Icons.search),
-              ),
-            )
+            _isSearch
+                ? const SizedBox.shrink()
+                : GestureDetector(
+                    onTap: () => _showSearchAppBar(context),
+                    child: Container(
+                      margin: const EdgeInsets.only(right: 16),
+                      width: 32,
+                      height: 32,
+                      child: const Icon(Icons.search),
+                    ),
+                  )
           ],
         ),
       );
---
+++ lib/shared/widgets/custom_search_bar.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+import '../../core/constants/app_colors.dart';
+
+class SearchInputTextfield extends StatefulWidget {
+  final TextEditingController controller;
+  final VoidCallback onClose;
+
+  const SearchInputTextfield({
+    super.key,
+    required this.controller,
+    required this.onClose,
+  });
+
+  @override
+  State<SearchInputTextfield> createState() => _SearchInputTextfieldState();
+}
+
+class _SearchInputTextfieldState extends State<SearchInputTextfield> {
+  @override
+  Widget build(BuildContext context) {
+    var searchBarPadding = 40;
+    return SizedBox(
+      width: MediaQuery.of(context).size.width - searchBarPadding,
+      height: 32,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Expanded(
+            child: TextField(
+              textInputAction: TextInputAction.search,
+              controller: widget.controller,
+              onSubmitted: (value) {
+                print(value);
+              },
+              style: const TextStyle(
+                fontFamily: 'Pretendard',
+                color: AppColors.grey500,
+                fontSize: 14,
+                fontWeight: FontWeight.w400,
+              ),
+              decoration: InputDecoration(
+                focusedBorder: OutlineInputBorder(
+                  // 포커스 상태에서의 border 색상
+                  borderRadius: const BorderRadius.all(Radius.circular(28)),
+                  borderSide: BorderSide(
+                    color: Colors.grey.shade400, // 포커스 상태에서의 border 색상
+                    width: 1.0, // 포커스 시 border 두께
+                  ),
+                ),
+                enabledBorder: OutlineInputBorder(
+                    borderRadius: const BorderRadius.all(Radius.circular(28)),
+                    borderSide: BorderSide(
+                      color: Colors.grey.shade400,
+                      width: 1.0,
+                    )),
+                prefixIcon: const Padding(
+                  padding: EdgeInsets.only(left: 8),
+                  child: Icon(
+                    Icons.search_rounded,
+                    color: AppColors.grey500,
+                  ),
+                ),
+                prefixIconConstraints: const BoxConstraints(
+                  minWidth: 20,
+                  minHeight: 20,
+                ),
+                hintStyle: TextStyle(
+                  color: Colors.grey.shade400,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400,
+                ),
+                hintText: '검색어를 입력해주세요',
+                contentPadding:
+                    const EdgeInsets.symmetric(horizontal: 4, vertical: 2),
+              ),
+            ),
+          ),
+          const SizedBox(
+            width: 8,
+          ),
+          GestureDetector(
+            onTap: widget.onClose,
+            child: const Text(
+              '취소',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w600,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
